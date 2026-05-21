c61723e79f612ae4cf968193679655ae5c804830
psy0635-ctrl <psy0635@gmail.com>
2026-05-21 13:00:41 +0900

    Java_study

--- src/ai0521/Rabbit/Rabbit.java
+++ src/ai0521/Rabbit/Rabbit.java
@@ -11,31 +11,37 @@ public class Rabbit {
     }
 
     public Rabbit(){
-
+            // 비어있음 = 초기값 없이 객체만 생성
     }
 
+    // shape 값을 꺼내줌
     public String getShape() {
-        return shape;
+        return shape;          // 저장된 shape 값 반환
     }
 
+    // shape 값을 받아서 저장
     public void setShape(String shape) {
-        this.shape = shape;
+        this.shape = shape; // this.shape = 필드, shape = 매개변수
     }
 
+    // xPos 값을 꺼내줌
     public int getxPos() {
-        return xPos;
+        return xPos;           // 저장된 xPos 값 반환
     }
 
+    // xPos 값을 받아서 저장
     public void setxPos(int xPos) {
-        this.xPos = xPos;
+        this.xPos = xPos; // this.xPos = 필드, xPos = 매개변수
     }
 
+    // yPos 값을 꺼내줌
     public int getyPos() {
-        return yPos;
+        return yPos;           // 저장된 yPos 값 반환
     }
 
+    // yPos 값을 받아서 저장
     public void setyPos(int yPos) {
-        this.yPos = yPos;
+        this.yPos = yPos; // this.yPos = 필드, yPos = 매개변수
     }
 }
 
--- src/ai0521/Rabbit/Run_Rabbit.java
+++ src/ai0521/Rabbit/Run_Rabbit.java
@@ -2,6 +2,22 @@ package ai0521.Rabbit;
 
 public class Run_Rabbit {
     public static void main(String[] args) {
+        // 1. 생성자 호출 → Rabbit 객체 생성
+        Rabbit r = new Rabbit();
 
+        // 3. setter로 값 저장
+        r.setShape("원형");      // shape = "원형"
+        r.setPosition(10, 20); // xPos = 10, yPos = 20
+
+        // 2. getter로 값 읽기
+        System.out.println("모양: "  + r.getShape());  // 원형
+        System.out.println("X위치: " + r.getxPos());  // 10
+        System.out.println("Y위치: " + r.getyPos());  // 20
+
+        // 위치 변경 후 다시 출력
+        r.setxPos(50);           // xPos = 50으로 변경
+        r.setyPos(100);          // yPos = 100으로 변경
+        System.out.println("변경 후 X: " + r.getxPos()); // 50
+        System.out.println("변경 후 Y: " + r.getyPos()); // 100
     }
 }
