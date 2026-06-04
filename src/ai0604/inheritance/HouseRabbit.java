1374246533edd0536e7776557175022f3b93eb62
psy0635-ctrl <psy0635@gmail.com>
2026-06-04 11:48:53 +0900

    Java_study

--- src/ai0604/inheritance/HouseRabbit.java
+++ src/ai0604/inheritance/HouseRabbit.java
@@ -35,4 +35,19 @@ public class HouseRabbit extends Rabbit {
     public void eatFeed(String feedName) {
         System.out.println("집토끼가 " + masterName + " 주인이 주는 " + feedName + " 사료를 먹는다.");
     }
+    //  메소드 재정의 : super 클래스에 정의된 메소드를 sub 클래스에서 다시 정의되는 것
+    // 울타리 x 좌표가 0을 벗어나지 않고 10씩 감소하게
+    @Override
+    public void moveLeftTen(){
+        if(xPos >= 10)
+            super.moveLeftTen();
+
+    }
+    // 울타리 x 좌표가 500을 벗어나지 않고 10씩 증가하게
+    @Override
+    public void MoveRightTen(){
+        if(xPos <= 490)
+            super.MoveRightTen();
+
+    }
 }
