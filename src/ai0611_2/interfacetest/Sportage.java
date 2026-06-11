892a84232edb66be52b162017fec0184a9c46e96
psy0635-ctrl <psy0635@gmail.com>
2026-06-11 11:46:21 +0900

    Java_study

--- src/ai0611_2/interfacetest/Sportage.java
+++ src/ai0611_2/interfacetest/Sportage.java
@@ -17,18 +17,18 @@ public class Sportage implements Car {
     @Override
     public void upSpeed(int speed) {
         this.speed += speed;
-        System.out.println(Carname + "자동차의 속도를 가속하여 현재 속도는" + this.speed + "입니다.");
+        System.out.println(Carname + "자동차의 속도를 가속하여 현재 속도는" + this.speed + " 입니다.");
     }
 
     @Override
     public void downSpeed(int speed) {
         this.speed += speed;
-        System.out.println(Carname + "자동차의 속도를 감속하여 현재 속도는" + this.speed + "입니다.");
+        System.out.println(Carname + "자동차의 속도를 감속하여 현재 속도는" + this.speed + " 입니다.");
     }
 
     @Override
     public void rotate(String direction) {
-        System.out.println(Carname + "자동차를" + direction + "회전한다");
+        System.out.println(Carname + "자동차를" + direction + " 회전한다");
     }
 
     @Override
